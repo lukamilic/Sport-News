46c3b2a94f679be5eed5bb22071f0a822d32c469
lukamilic <luka.milic77@gmail.com>
2017-01-30 17:22:44 +0100

    Initial Commit

---
+++ Sports News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sports News
+//
+//  Created by Luka Milic on 1/30/17.
+//  Copyright © 2017 Luka Milic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
